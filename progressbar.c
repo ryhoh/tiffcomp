694b37789be2c2882158cc13d5b6682de777baec
ryhoh <axis38akasira@gmail.com>
2016-12-18 17:46:16 +0900

    Written before starting github

--- progressbar.c
+++ progressbar.c
@@ -1,43 +1,4 @@
-/*
-The MIT License (MIT)
-Copyright (c) 2016 tetsuya
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-associated documentation files (the "Software"), to deal in the Software without restriction,
-including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
-BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. 
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-
-/*
- *		progressbar.c
- *
- *		int printProgress(int pos, int full)
- *
- *		int pos		完了した作業工程の数
- *		int full	全作業工程の数
- *
- *		入力されるposは常に		0 <= pos <= full	を満たす
- *
- *		表示成功で0、失敗で1を返す
- *
- *
- *		表示例	（全工程1000個中、334個完了の状態）
- *
- *		|******--------------| 33.4%    NUM:[ 334 / 1000 ]
- *
- *		表示部は20文字からなる21段階で、5%完了する毎に*が追加される
- *		百分率は右詰め5桁（.以下1桁）表記、空きは空白で埋める
- *
- */
+// 堀哲也 2016/11/05
 
 #include <stdio.h>
 
